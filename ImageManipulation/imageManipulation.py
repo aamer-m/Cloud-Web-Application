4313397ede580076c927669817e443317355b8ce
mohammed aamer <Aamer@ramm-mbp.local>
2016-03-12 20:53:30 -0500

    Initial commit.

---
+++ ImageManipulation/imageManipulation.py
@@ -0,0 +1,87 @@
+from google.appengine.api import images
+import PIL
+from PIL import Image
+import mimetypes
+from StringIO import StringIO
+import cStringIO
+from google.appengine.api import urlfetch
+import ImageEnhance
+import webapp2
+
+
+# Entry point of the application
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write('<html><head></head><body>')
+        self.response.out.write("""
+        <form method="POST" action="/upload_photo" target="otp">
+        <label>Image URL: <input type="url" name="imageURL" placeholder="Enter an image URL here"></label><br>
+        <label>Rotate: <input type="range" name="rotate" min="0" max="360" value="0" step="1"  onchange="showValue(this.name, this.value)"/>
+        <span id="rotate">0</span></label><br>
+        <script type="text/javascript">
+        function showValue(identity, newValue)
+        {
+            document.getElementById(identity).innerHTML=newValue
+        }
+        </script>
+        <label>Color: <input type="range" name="color" min="-2" max="2" value="1" step="0.1" onchange="showValue(this.name, this.value)" />
+        <span id="color">1</span></label><br>
+
+        <label>Contrast: <input type="range" name="contrast" min="0" max="3" value="1" step="0.1" onchange="showValue(this.name, this.value)" />
+        <span id="contrast">1</span></label><br>
+        
+        <label>Sharpness: <input type="range" name="sharpness" min="-2" max="2" value="1" step="0.1" onchange="showValue(this.name, this.value)" />
+        <span id="sharpness">1</span></label><br>
+        
+        
+        <label>Brightness: <input type="range" name="brightness" min="0" max="4" value="1" step="0.1" onchange="showValue(this.name, this.value)" />
+        <span id="brightness">1</span></label><br>
+
+        <h5><label><input type="checkbox" name="lucky" value="I'm Feeling Lucky"/> If you have any problem setting the values, Select this and we will get you a "I'm Feeling Lucky" image <label></h5>
+        
+        <input type="submit" name="submit"  value="submit"/><br>
+        <iframe name="otp" frameborder="0" width="100%" height="100%"></iframe>
+        </form>
+        </body>
+        </html>""")
+
+class ImageHandler(webapp2.RequestHandler):
+    def post(self):
+        gurl = self.request.get('imageURL')
+        gcolor = float(self.request.get('color'))
+        gbrightness = float(self.request.get('brightness'))
+        gcontrast = float(self.request.get('contrast'))
+        gsharpness = float(self.request.get('sharpness'))
+        grotate = int(self.request.get('rotate'))
+        gImFLSelected = bool(self.request.get('lucky'))
+        c = urlfetch.fetch(gurl).content
+        im = Image.open(StringIO(c))
+        mimeType = im.format
+        if gImFLSelected:
+            img = images.Image(c)
+            print('helo')
+            img.im_feeling_lucky()
+            data = img.execute_transforms(output_encoding=images.JPEG)
+        else: 
+            enh = ImageEnhance.Color(im) # 0 - 2 to be considered
+            out = enh.enhance(gcolor)
+            enh = ImageEnhance.Brightness(out) # 0 - black image, 1 - original image; Can give more than 1.0
+            out = enh.enhance(gbrightness)
+            enh = ImageEnhance.Contrast(out) # 0 - solid grey image, 1 - original image
+            out = enh.enhance(gcontrast)
+            enh = ImageEnhance.Sharpness(out) # 0 - blurred image, 1 - original image, 2 - sharpened image
+            out = enh.enhance(gsharpness) 
+            out = out.rotate(grotate, resample=Image.BICUBIC, expand=True)
+            buf = cStringIO.StringIO()
+            out.save(buf, mimeType)
+            data = buf.getvalue()
+    
+            
+        self.response.headers['Content-Type'] = 'image/' + mimeType
+        self.response.out.write(data)
+        
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage), 
+    ('/upload_photo', ImageHandler),
+], debug=True)
---
+++ README.md
@@ -0,0 +1,35 @@
+Image Manipulation Web App
+==========================
+
+URL: http://<to be deployed>.appspot.com/
+------------------------------------
+
+> Created a Google App Engine application 
+> to perform image manipulations submitted by the user
+> using URL with additional parameters. User can rotate
+> the image, change the Contrast, Sharpness, Brightness and
+> i_m_feeling_lucky feature provided by Image API which adjusts
+> image contrasts and color levels.
+
+# Used AppEngine web services
+ - Image Transformation API (with PIL)
+ - Google Cloud Datastore
+   - NDB Datastore API
+ - *
+ - *
+
+
+# Version
+1.0
+
+# Example
+An Sample image url is submitted by the user.
+
+ - Image:
+![alt bat]
+
+ - Web app homepage
+![image homepage]
+
+ - Web app output
+![image result]
